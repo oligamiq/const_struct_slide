11940d7819290083293db56b77ab1b6394041a4e
oligami <s20032@tokyo.kosen-ac.jp>
2024-08-29 18:30:10 +0900

    Updates

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    base: process.env.NODE_ENV === "production" ? "/const_struct_slide/" : "/",
-});
