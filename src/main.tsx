6c6430ea606e0aed14dc2ab914637a3dedf1a820
oligami <s20032@tokyo.kosen-ac.jp>
2024-08-29 18:28:54 +0900

    save

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,6 @@ import "./index.css";
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+        <App />
 	</React.StrictMode>,
 );
